1e7ec2eba83fb090874bc1e8992e6c75d174b954
bbasvani <bhagavan.basvani@capgemini.com>
2020-12-25 17:20:26 +0530

    pushing code to git

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using aspnetcore_config_app.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+// Added
+using Microsoft.Extensions.Configuration;
+
+namespace aspnetcore_config_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        // Added
+        private readonly IConfiguration _config;
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)// Added
+        {
+            _logger = logger;
+            // Added
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            // Added
+            string currentAppEnviroment = _config["appEnviroment"];
+            ViewData["envValue"] = currentAppEnviroment;
+            return View(ViewData["envValue"]);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
